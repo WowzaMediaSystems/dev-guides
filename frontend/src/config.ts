54761c58a2cbc30cff21fb31354d885c7e720fa2
Steven Yi <stevenyix@users.noreply.github.com>
2025-10-01 07:49:39 -0700

    config.ts

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,17 +1,8 @@
 export const FLOWPLAYER_TOKEN = import.meta.env.FLOWPLAYER_TOKEN 
 
-
-// Stream URL to use in /obj-detection-ibc page
 export const DEFAULT_OBJ_DETECTION_STREAM_URL = "https://triviaelephant.s3.amazonaws.com/id3/testhls/playlist.m3u8";
 
-// Stream URL to use in /transcription page
-// export const DEFAULT_TRANSCRIPTION_STREAM_URL = "https://triviaelephant.s3.amazonaws.com/id3/barry/playlist.m3u8";
-
-// export const DEFAULT_TRANSCRIPTION_STREAM_URL = "https://wse-trial.wowza.com/whisper/myStream/playlist.m3u8";
-
 export const DEFAULT_TRANSCRIPTION_STREAM_URL ="https://wse-demo.wowza.com/whisper/myStream_delayed/playlist.m3u8"
-
-// export const DEFAULT_LIVESTREAM_URL ="https://wse-demo.wowza.com/live/_definst_/myStream/playlist.m3u8"
 
 export const DEFAULT_LIVESTREAM_URL = "http://localhost:1935/live/_definst_/myStream/playlist.m3u8"
 
