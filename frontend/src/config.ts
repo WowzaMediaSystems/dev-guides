85d171c7bae2cbe4354212ceaefa28a6e77eebfb
Steven Yi <stevenyix@users.noreply.github.com>
2025-11-05 14:19:11 -0800

    cleanup

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,7 +2,10 @@ export const FLOWPLAYER_TOKEN = import.meta.env.FLOWPLAYER_TOKEN
 
 export const DEFAULT_OBJ_DETECTION_STREAM_URL = "https://triviaelephant.s3.amazonaws.com/id3/testhls/playlist.m3u8";
 
+// export const DEFAULT_TRANSCRIPTION_STREAM_URL ="https://wse-demo.wowza.com/whisper/myStream_delayed/playlist.m3u8"
+
 export const DEFAULT_TRANSCRIPTION_STREAM_URL ="https://wse-demo.wowza.com/whisper/myStream_delayed/playlist.m3u8"
+
 
 export const DEFAULT_LIVESTREAM_URL = "http://localhost:1935/live/_definst_/myStream/playlist.m3u8"
 
